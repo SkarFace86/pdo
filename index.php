8d9f481dec9aefa6feb73d22b5acd700e714e09b
Brandon Skar <bransk01@yahoo.com>
2019-02-11 17:15:29 -0500

    Updated index to make is more secure

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
  * Time: 2:09 PM
  */
 
+require_once('/home/bskargre/config.php');
 //Connect to DB
 try {
     //instantiate a database object
-    $dbh = new PDO("mysql:dbname=bskargre_grc", "bskargre", "QWQWqwqw1!");
+    $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
     echo 'Connected to database!';
 } catch (PSOEcception $e) {
     echo $e->getMessage();
