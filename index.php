c2420a2617b0a413c57e64e35214e034f8c96659
Brandon Skar <bransk01@yahoo.com>
2019-02-11 17:21:54 -0500

    Added a purple kangaroo named Joey to the database

--- index.php
+++ index.php
@@ -11,7 +11,24 @@ require_once('/home/bskargre/config.php');
 try {
     //instantiate a database object
     $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-    echo 'Connected to database!';
 } catch (PSOEcception $e) {
     echo $e->getMessage();
 }
+
+//define the query
+$sql = "INSERT INTO pets(type, name, color)
+    VALUES (:type, :name, :color)";
+
+//prepare the statement
+$statement = $dbh->prepare($sql);
+
+//Bind the parameters
+$type = 'kangaroo';
+$name = 'Joey';
+$color = 'purple';
+$statement->bindParam(':type', $type, PDO::PARAM_STR);
+$statement->bindParam(':name', $name, PDO::PARAM_STR);
+$statement->bindParam(':color', $color, PDO::PARAM_STR);
+
+//execute
+$statement->execute();
