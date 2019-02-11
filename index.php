0ad09e6d0767a79372ff6778f6c86a72da7dede4
Brandon Skar <bransk01@yahoo.com>
2019-02-11 17:37:46 -0500

    Changed color of alpaca to brown

--- index.php
+++ index.php
@@ -60,17 +60,19 @@ $id = $dbh->lastInsertId();
 echo "<p>Pet $id inserted successfully.</p>";*/
 
 //define the query
-$sql = "UPDATE pets SET name = :new
-    WHERE name = :old";
+$sql = "UPDATE pets SET color = :new
+    WHERE name = :name AND type = :type";
 
 //prepare the statement
 $statement = $dbh->prepare($sql);
 
 //bind the parameters
-$old = 'Joey';
-$new = 'Troy';
-$statement->bindParam(':old', $old, PDO::PARAM_STR);
+$new = 'brown';
+$name = 'Oscar';
+$type = 'alpaca';
 $statement->bindParam(':new', $new, PDO::PARAM_STR);
+$statement->bindParam(':name', $name, PDO::PARAM_STR);
+$statement->bindParam(':type', $type, PDO::PARAM_STR);
 
 //execute
 $statement->execute();
